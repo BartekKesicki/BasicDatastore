bcdb73dcc0684a8e29676e94f3ded20ce23fdd41
BartekK <bartek.kesicki25@gmail.com>
2023-04-24 22:41:12 +0200

    remove jetpack compose

--- app/src/main/java/com/example/basicdatastore/activities/MainActivity.kt
+++ app/src/main/java/com/example/basicdatastore/activities/MainActivity.kt
@@ -2,29 +2,11 @@ package com.example.basicdatastore.activities
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.basicdatastore.ui.theme.BasicDatastoreTheme
-import com.example.basicdatastore.widgets.BasicDatastoreBody
+import com.example.basicdatastore.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BasicDatastoreTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    BasicDatastoreBody()
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
